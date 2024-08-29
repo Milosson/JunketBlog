2c6a9c86c7076c3a3df4c94e5411156a79df329e
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-29 12:43:29 +0000

    DEBUG

--- JunketBlog_main/settings.py
+++ JunketBlog_main/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-milosson-junketblog-ywf6d91d9c8.ws-eu115.gitpod.io',
 '.herokuapp.com'
