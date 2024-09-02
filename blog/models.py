262db73577e67f34a1d44a0a2b798401a5a9dad8
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-09-02 15:46:30 +0000

    Update TravelPost model to include travel_date and location fields; add initial_data.yaml fixture

--- blog/models.py
+++ blog/models.py
@@ -11,18 +11,19 @@ class TravelPost(models.Model):
     title = models.CharField(max_length=200, unique=True)  # Title of the travel post
     slug = models.SlugField(max_length=200, unique=True)  # Slug for a URL-friendly representation
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="travel_posts"  # Author of the post
-        ) 
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="travel_posts"  # Author of the post
+    ) 
     content = models.TextField()  # Main content of the travel post
     created_on = models.DateTimeField(auto_now_add=True)  # Timestamp for when the post was created
     updated_on = models.DateTimeField(auto_now=True)  # Timestamp for the last update of the post
     status = models.IntegerField(choices=STATUS_CHOICES, default=0)  # Status of the post (draft or published)
-    excerpt = models.TextField(blank=True) 
-   # image = models.ImageField(upload_to='images/', null=True, blank=True)  # User-uploaded image for the post
+    excerpt = models.TextField(blank=True)  # Short summary of the post
+    # image = models.ImageField(upload_to='images/', null=True, blank=True)  # User-uploaded image for the post
     location = models.CharField(max_length=100)  # Location related to the travel post
     travel_date = models.DateField()  # Date of the travel
     tags = models.ManyToManyField('Tag', blank=True)  # Tags to categorize the travel post
-    
 
     class Meta:
         ordering = ['-created_on']  # Order posts by creation date, newest first
