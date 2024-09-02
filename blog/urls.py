fcd459a95973d7befa7d931095c88d34f0e366aa
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-09-02 16:59:40 +0000

    Updated URLs and views, added index.html and base.html, modified settings.py for templates configuration

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),  # Home page for all posts
+    path('', views.post_list, name='home'),  # Home page for all posts
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),  # Detail view for a single post
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import TravelPost
 
@@ -8,4 +9,7 @@ def post_detail(request, slug):
 
 def post_list(request):
     posts = TravelPost.objects.filter(status=1).order_by('-created_on')  # Fetch all TravelPost objects
-    return render(request, 'blog/travelpost_list.html', {'posts': posts})  # Render the list template
+    paginator = Paginator(posts, 6)  # Show 6 posts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/index.html', {'posts': page_obj.object_list, 'is_paginated': page_obj.has_other_pages(), 'page_obj': page_obj})
